8bfa9465c5042ec11c91013b040818e7a30f889a
2962283762 <2962283762@qq.com>
2024-06-18 19:35:59 +0800

    2024-06-18 19:35

---
+++ BasicVideoCall/app/src/main/java/com/ding/rtc/demo/basicvideocall/DingApplication.java
@@ -0,0 +1,56 @@
+package com.ding.rtc.demo.basicvideocall;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.multidex.MultiDex;
+
+import com.ding.rtc.DingRtcEngine;
+
+public class DingApplication extends Application {
+
+    public static final String APP_ID = "byhbqw5p"; // 应用ID
+    public static final String TOKEN = "000eJxjYGBQiFz9yXZVv7ppzsFJ3zVPLPx8/7CZ+oeS2MZb2pdUTTPndTAwMHAkVWYkFZabFqQVFgTFFm4WTis6eAkozpySkgKiEpOSGRgZRsGQAwAChxxQ"; // 频道鉴权令牌Token
+    public static final String GSLB_SERVER = ""; // GSLB地址
+    private DingRtcEngine mRtcEngine;
+    private final DingEngineCallback mRtcCallback = new DingEngineCallback();
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        initDingEngine();
+    }
+
+    public DingRtcEngine getDingEngine() {
+        if (mRtcEngine == null) {
+            initDingEngine();
+        }
+        return mRtcEngine;
+    }
+
+    public void registerEventHandler(EventHandler handler) { mRtcCallback.addHandler(handler); }
+    public void removeEventHandler(EventHandler handler) { mRtcCallback.removeHandler(handler); }
+
+    public void initDingEngine() {
+        if (mRtcEngine == null) {
+            mRtcEngine = DingRtcEngine.create(getApplicationContext(), "");
+            mRtcEngine.subscribeAllRemoteAudioStreams(true);
+            mRtcEngine.subscribeAllRemoteVideoStreams(false);
+            mRtcEngine.setRemoteDefaultVideoStreamType(DingRtcEngine.DingRtcVideoStreamType.DingRtcVideoStreamTypeFHD);
+            mRtcEngine.setRtcEngineEventListener(mRtcCallback);
+        }
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        mRtcEngine.destroy();
+    }
+}
